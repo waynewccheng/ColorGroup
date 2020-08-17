f6511c6a2e18b2c206bf3829885449da020c9586
plemaillet <paul.lemaillet@gmail.com>
2020-08-17 14:49:46 -0400

    BRDR Rotating Camera First Push
    Rotation camera, screenshot images saved in Assets/ScreenShots

---
+++ unity/BRDF_RotatingCamera/Assets/Scripts/RotatingFrame.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using UnityEngine;
+
+public class RotatingFrame : MonoBehaviour
+{
+    private static RotatingFrame instance;
+    public Camera rotatingCamera;
+    public ScreenshotHandler rotcameraScreenShot;
+
+    const float degreesPerStep = 10f, cameraAngleStartPosition = -80f;
+
+    public int cameraIndexPosition = 0;
+    public int cameraIndexMax = 17;
+    float timer = 0f;
+
+    private void Awake()
+    {
+        instance = this;
+
+        rotatingCamera = GameObject.Find("rotatingCamera").GetComponent<Camera>();
+        rotcameraScreenShot = rotatingCamera.GetComponent<ScreenshotHandler>();
+    }
+
+    void UpdateDiscrete()
+    {
+        // Frame rotation angle
+        float angle = cameraAngleStartPosition + cameraIndexPosition * degreesPerStep;
+
+        // Update cameraYRotation for name of savedimage 
+        rotcameraScreenShot.cameraYRotation = angle;
+
+        if (cameraIndexPosition < cameraIndexMax)
+        {
+            //Debug.Log(cameraIndexPosition);
+            transform.localRotation = Quaternion.Euler(0f,
+                cameraAngleStartPosition + cameraIndexPosition * degreesPerStep, 0f);
+            rotcameraScreenShot.TakeScreenshots(500, 500);
+            cameraIndexPosition += 1;
+        }
+    }
+
+
+    public void LaunchRotatingCamera()
+    {
+        // First camera position, no waiting time
+        if (cameraIndexPosition == 0)
+            UpdateDiscrete();
+        else
+        {
+            // Timer
+            timer += Time.deltaTime;
+
+            if (timer > 1)
+            {
+                UpdateDiscrete();
+                timer = 0f;
+            }
+        }
+    }
+}
